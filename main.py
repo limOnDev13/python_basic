a5a93e43d21f69f0ab6b898a9ff3763ce0d2c70f
Vladimir <volosnikovvladimir69@gmail.com>
2024-01-26 15:21:38 +0500

    Выполнил 1 задание. Кстати, можно ли писать коммиты на русском?

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print('Hello world!')
-print('Second string')
+import platform
+import sys
+
+info = 'OS info is \n{}\n\nPython version is {} {}'.format(
+    platform.uname(),
+    sys.version,
+    platform.architecture(),
+)
+print(info)
+
+with open('os_info.txt', 'w', encoding='utf8') as file:
+    file.write(info)

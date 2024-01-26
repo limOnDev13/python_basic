fb0f739a07de6061900632d7d527195aa79f5750
Vladimir <volosnikovvladimir69@gmail.com>
2024-01-26 15:44:19 +0500

    Выполнил задание 3

--- Module14/03_least_divisor/main.py
+++ Module14/03_least_divisor/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+def smallest_divisor(number: int) -> int:
+    for num in range(2, number + 1):
+        if number % num == 0:
+            return num
+
+
+n: int = int(input('Введите число: '))
+
+print('Наименьший делитель, отличный от единицы:', smallest_divisor(n))

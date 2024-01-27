11335873f5f623d218aef5a35d67fd18d3590dbc
Vladimir <volosnikovvladimir69@gmail.com>
2024-01-27 12:52:00 +0500

    Пытаюсь перенести коммиты из мастера

--- Module15/01_list_gen/main.py
+++ Module15/01_list_gen/main.py
@@ -1,1 +1,8 @@
+<<<<<<< HEAD
 # TODO здесь писать код
+=======
+number: int = int(input('Введите число: '))
+
+res_numbers = [num for num in range(1, number + 1, 2)]
+print('Список из нечетных чисел от одного до N:', res_numbers)
+>>>>>>> 55f02ed (done task 2 (02_tournament\main.py), 15.1 fix: an extra comment in the code has been removed)

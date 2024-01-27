74962d927ab9d85dc8e52f9d3df0aa18b0e9a71a
Vladimir <volosnikovvladimir69@gmail.com>
2024-01-27 19:08:06 +0500

    Revert "done task 3 (03_videocards/main.py)"
    This reverts commit 6e0c02ea0f3351558e349982281c2d7133549ad1.

--- Module15/03_videocards/main.py
+++ Module15/03_videocards/main.py
@@ -1,14 +1,3 @@
-number_video_cards: int = int(input('Количество видеокарт: '))
-models: list[int] = list()
+# TODO здесь писать код
 
-for i in range(number_video_cards):
-    model: int = int(input(f'{i + 1} Видеокарта: '))
-    models.append(model)
 
-print('\nСтарый список видеокарт:', models)
-
-oldest_model: int = max(models)
-while oldest_model in models:
-    models.pop(models.index(oldest_model))
-
-print('Новый список видеокарт:', models)

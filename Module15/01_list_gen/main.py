c13a2b6a140317bb6b7728e183fa978de262d626
Vladimir <volosnikovvladimir69@gmail.com>
2024-01-27 19:08:07 +0500

    Revert "added m15 homework, task 15.1"
    This reverts commit 7a7826c8e8eb6a9d40aff42e5b715c6617be8684.

--- Module15/01_list_gen/main.py
+++ Module15/01_list_gen/main.py
@@ -1,5 +1,1 @@
 # TODO здесь писать код
-number: int = int(input('Введите число: '))
-
-res_numbers = [num for num in range(1, number + 1, 2)]
-print('Список из нечетных чисел от одного до N:', res_numbers)

7a7826c8e8eb6a9d40aff42e5b715c6617be8684
Vladimir <volosnikovvladimir69@gmail.com>
2024-01-27 12:43:37 +0500

    added m15 homework, task 15.1

--- Module15/01_list_gen/main.py
+++ Module15/01_list_gen/main.py
@@ -1,1 +1,5 @@
 # TODO здесь писать код
+number: int = int(input('Введите число: '))
+
+res_numbers = [num for num in range(1, number + 1, 2)]
+print('Список из нечетных чисел от одного до N:', res_numbers)

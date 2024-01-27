b1ba3625c3b7aac7f365150beb18a69cae194e08
Vladimir <volosnikovvladimir69@gmail.com>
2024-01-27 13:02:25 +0500

    done task 3 (03_videocards/main.py)

--- Module15/03_videocards/main.py
+++ Module15/03_videocards/main.py
@@ -1,3 +1,14 @@
-# TODO здесь писать код
+number_video_cards: int = int(input('Количество видеокарт: '))
+models: list[int] = list()
 
+for i in range(number_video_cards):
+    model: int = int(input(f'{i + 1} Видеокарта: '))
+    models.append(model)
 
+print('\nСтарый список видеокарт:', models)
+
+oldest_model: int = max(models)
+while oldest_model in models:
+    models.pop(models.index(oldest_model))
+
+print('Новый список видеокарт:', models)

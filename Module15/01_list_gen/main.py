55f02ed55e1963b26ee173f225fa878518b4718a
Vladimir <volosnikovvladimir69@gmail.com>
2024-01-27 12:52:00 +0500

    done task 2 (02_tournament\main.py), 15.1 fix: an extra comment in the code has been removed

--- Module15/01_list_gen/main.py
+++ Module15/01_list_gen/main.py
@@ -1,4 +1,3 @@
-# TODO здесь писать код
 number: int = int(input('Введите число: '))
 
 res_numbers = [num for num in range(1, number + 1, 2)]
--- Module15/02_tournament/main.py
+++ Module15/02_tournament/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+list_names: list[str] = ['Артемий', 'Борис',
+                         'Влад', 'Гоша', 'Дима',
+                         'Евгений', 'Женя', 'Захар']
+new_list_names: list[str] = list()
+
+for index, name in enumerate(list_names):
+    if index % 2 == 0:
+        new_list_names.append(name)
+
+print('Первый день:', new_list_names)

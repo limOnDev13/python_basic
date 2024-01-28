5121309bc6b304e4c2b8abed6dc1673a31d62ffe
Vladimir <volosnikovvladimir69@gmail.com>
2024-01-28 11:39:13 +0500

    Done task 1

--- Module16/01_scary_code/main.py
+++ Module16/01_scary_code/main.py
@@ -1,24 +1,16 @@
-a = [1, 5, 3]
-b = [1, 5, 1, 5]
-c = [1, 3, 1, 5, 3, 3]
-for i in b:
-    a.append(i)
-t = 0
-for i in a:
-    if i == 5:
-        t += 1
-print(t)
-d = []
-for i in a:
-    if i != 5:
-        d.append(i)
-for i in c:
-    d.append(i)
-t = 0
-for i in d:
-    if i == 3:
-        t += 1
-print(t)
-print(d)
+print('Результат работы программы:')
+main: list[int] = [1, 5, 3]
+first_side_list: list[int] = [1, 5, 1, 5]
+second_side_list: list[int] = [1, 3, 1, 5, 3, 3]
 
-# TODO переписать программу
+main.extend(first_side_list)
+num5: int = main.count(5)
+print('Кол-во цифр 5 при первом объединении:', num5)
+for _ in range(num5):
+    main.remove(5)
+
+main.extend(second_side_list)
+num3: int = main.count(3)
+print('Кол-во цифр 3 при втором объединении:', num3)
+
+print('Итоговый список:', main)

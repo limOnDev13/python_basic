d386b794f57f01161adef7827f525c607bbea4b3
Vladimir <volosnikovvladimir69@gmail.com>
2024-01-30 23:41:09 +0500

    Done task 7

--- Module17/07_list_of_lists_2/main.py
+++ Module17/07_list_of_lists_2/main.py
@@ -1,4 +1,10 @@
 nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
              [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+result_list: list[int] = [
+    number
+    for first_list in nice_list
+    for second_list in first_list
+    for number in second_list
+]
+print(result_list)

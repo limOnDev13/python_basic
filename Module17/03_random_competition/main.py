c0a7eb78aa675b983108ea00d0f791a6d5e7795a
Vladimir <volosnikovvladimir69@gmail.com>
2024-01-30 22:22:21 +0500

    Done task 3

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+from random import uniform
+
+
+length: int = 20
+first_team: list[float] = [round(uniform(5, 10), 2) for _ in range(length)]
+second_team: list[float] = [round(uniform(5, 10), 2) for _ in range(length)]
+
+winners: list[float] = [(first_team[i] if first_team[i] > second_team[i] else second_team[i]) for i in range(length)]
+
+print('Первая команда:', first_team)
+print('Вторая команда:', second_team)
+print('Победители тура:', winners)

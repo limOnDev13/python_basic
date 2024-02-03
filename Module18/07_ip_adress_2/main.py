789a624983b54d6b71b7b27d47d14dd43a83c49a
Vladimir <volosnikovvladimir69@gmail.com>
2024-02-03 15:06:13 +0500

    done task 7

--- Module18/07_ip_adress_2/main.py
+++ Module18/07_ip_adress_2/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+ip_address: str = input('Введите IP: ')
+
+numbers: list[str] = ip_address.split('.')
+# Контроль количества
+if len(numbers) != 4:
+    print('Адрес — это четыре числа, разделённые точками.')
+    exit()
+
+# Контроль типа данных и их корректность
+for num in numbers:
+    if not num.isdigit():
+        print('{} - это не целое число.'.format(num))
+        break
+    elif int(num) < 0:
+        print('{} меньше 0'.format(num))
+        break
+    elif int(num) > 255:
+        print('{} превышает 255.'.format(num))
+        break
+else:
+    print('IP-адрес корректен.')

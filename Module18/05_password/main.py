841862cae04a4915728cbf8fd107e001f8817738
Vladimir <volosnikovvladimir69@gmail.com>
2024-02-03 14:25:01 +0500

    done task 5

--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -1,1 +1,26 @@
-# TODO здесь писать код
+def filter_for_password(password: str) -> bool:
+    # Проверка на длину
+    if len(password) < 8:
+        return False
+
+    # Проверка на наличие заглавной буквы и количество цифр
+    count_nums: int = 0
+    has_upper_letter: bool = False
+    for letter in password:
+        if letter.isupper():
+            has_upper_letter = True
+        if letter.isdigit():
+            count_nums += 1
+
+    if count_nums < 3 or not has_upper_letter:
+        return False
+    return True
+
+
+text: str = input('Придумайте пароль: ')
+
+while not filter_for_password(text):
+    text = input('Пароль ненадёжный. Попробуйте ещё раз.\n'
+                 'Придумайте пароль: ')
+
+print('Это надежный пароль!')

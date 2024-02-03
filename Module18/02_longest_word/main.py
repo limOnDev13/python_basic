5ef957e3adfe5aa5037114af2426b2468c175c3d
Vladimir <volosnikovvladimir69@gmail.com>
2024-02-03 13:57:44 +0500

    done task 2

--- Module18/02_longest_word/main.py
+++ Module18/02_longest_word/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+text: str = input('Введите строку: ')
+
+words: list[str] = text.split()
+largest_word: str = max(words, key=lambda x: len(x))
+
+print('Самое длиное слово: {word}\nДлина этого слова: {length}'.format(
+    word=largest_word,
+    length=len(largest_word)
+))

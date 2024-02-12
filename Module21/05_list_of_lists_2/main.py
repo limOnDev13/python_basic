482dd2c65ba443781975d435cebae5f81a90bb43
Vladimir <volosnikovvladimir69@gmail.com>
2024-02-12 20:10:23 +0500

    done task 5

--- Module21/05_list_of_lists_2/main.py
+++ Module21/05_list_of_lists_2/main.py
@@ -1,4 +1,21 @@
+def expand_lists(lists: list) -> list[int]:
+    """
+    Функция рекурсивно раскрывает все вложенные списки.
+    :param lists: Список элементов.
+    :return: Раскрытый список.
+    """
+    result_list: list[int] = list()
+
+    for elem in lists:
+        if isinstance(elem, list):
+            result_list.extend(expand_lists(elem))
+        else:
+            result_list.append(elem)
+
+    return result_list
+
+
 nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
              [[11, 12, 13], [14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+print(expand_lists(nice_list))

9ee6e3bc8b40a4d45ac4bc38891c7b8617d0350c
Vladimir <volosnikovvladimir69@gmail.com>
2024-02-24 22:18:18 +0500

    Done task 5. Previous commit - wrong, done task 4

--- Module23/05_reliable_calculations/main.py
+++ Module23/05_reliable_calculations/main.py
@@ -1,6 +1,25 @@
-# TODO здесь писать код
+import math
+from typing import Any, SupportsFloat
 
-# Здесь создайте функцию get_sage_sqrt
+
+def get_sage_sqrt(num: Any) -> float:
+    """
+    Функция выполняет безопасное извлечение квадратного корня.
+    :param num: Число.
+    :return: Квадратный корень.
+    """
+    try:
+        if not isinstance(num, SupportsFloat):
+            raise TypeError(f'{num} не является типом,'
+                            f' из которого можно извлечь квадратный корень')
+        elif num < 0:
+            raise ValueError(f'{num} меньше 0!')
+        return math.sqrt(num)
+    except (TypeError, ValueError) as exc:
+        print(exc)
+    except Exception:
+        print('Непредвиденная ошибка!')
+
 
 
 # Тестовые случаи

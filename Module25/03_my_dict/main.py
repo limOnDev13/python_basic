129b0e2fb268d5aac691683a743903d33240c2d9
Vladimir <volosnikovvladimir69@gmail.com>
2024-03-08 11:37:52 +0500

    done task 3

--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+from typing import Any
+
+
+class MyDict(dict):
+    def get(self, key):
+        item: Any | None = super().get(key)
+        if item is None:
+            return 0
+        else:
+            return item
+
+
+my_dict: MyDict = MyDict()
+print(my_dict.get(1))

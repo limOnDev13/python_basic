878f075a5d46970dcfe546ffc4d636c5aa8d91ce
Vladimir <volosnikovvladimir69@gmail.com>
2024-03-19 19:06:57 +0500

    Done task 1

--- Module27/01_how_are_you/main.py
+++ Module27/01_how_are_you/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+from typing import Callable
+
+
+def how_are_you(func: Callable) -> Callable:
+    """
+    Декоратор. Перед выполнением любой функции спрашивает как дела и в независимости от ответа пишет в консоль
+    'А у меня не очень! Ладно, держи свою функцию.'
+    :param func: Оборачиваемая функция
+    :type func: Callable
+    :return: func без изменений
+    :rtype: Callable
+    """
+    input('Как дела? ')
+    print('А у меня не очень! Ладно, держи свою функцию.')
+    return func
+
+
+@how_are_you
+def test() -> None:
+    """Тестовая функция"""
+    print('<Тут что-то происходит...>')
+
+
+test()

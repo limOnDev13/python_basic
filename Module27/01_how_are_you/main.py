943eaabf739310b97b7999fa311a61e70b9cc42a
Vladimir <volosnikovvladimir69@gmail.com>
2024-03-19 19:44:04 +0500

    Fixed decorator how_are_you

--- Module27/01_how_are_you/main.py
+++ Module27/01_how_are_you/main.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def how_are_you(func: Callable) -> Callable:
@@ -10,15 +10,22 @@ def how_are_you(func: Callable) -> Callable:
     :return: func без изменений
     :rtype: Callable
     """
-    input('Как дела? ')
-    print('А у меня не очень! Ладно, держи свою функцию.')
-    return func
+    def wrapper(*args, **kwargs) -> Any:
+        """
+        Обертка для func
+        :return: Результат работы func
+        :rtype: Any
+        """
+        input('Как дела? ')
+        print('А у меня не очень! Ладно, держи свою функцию.')
+        return func(*args, **kwargs)
+    return wrapper
 
 
 @how_are_you
-def test() -> None:
+def test(x: int) -> None:
     """Тестовая функция"""
     print('<Тут что-то происходит...>')
 
 
-test()
+test(1)

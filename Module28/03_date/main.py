c525217d7b66a6aefc0cfe674219d4539131cb6d
Vladimir <volosnikovvladimir69@gmail.com>
2024-03-23 07:49:30 +0500

    Started writing class Date

--- Module28/03_date/main.py
+++ Module28/03_date/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+from abc import ABC
+from typing import Optional
+
+
+class Date(ABC):
+    def __init__(self):
+        self.__day: Optional[int] = None
+        self.__month: Optional[int] = None
+        self.__year: Optional[int] = None
+
+    @classmethod
+    def from_string(cls, date_string: str) -> 'Date':
+        return self
+
+
+date: Date = Date.from_string('123', '12321')
